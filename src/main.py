2cfcba3af0a6971cdfe6261e1ac2ccb2bef808ac
juchim2005 <63352963+juchim2005@users.noreply.github.com>
2025-05-19 18:56:56 +0200

    basic flask template

--- src/main.py
+++ src/main.py
@@ -1,6 +1,34 @@
 from shoppingList import *
 from product import *
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 shoppinglist = ShoppingList()
 product = Product("milk",1.99,"dairy",False)
+product2 = Product("eggs",0.99,"dairy",False)
 shoppinglist.add_product(product,2)
+shoppinglist.add_product(product2,3)
+product.price = 2.99
+shoppinglist.edit_product(product,3,True)
 print(shoppinglist.products)
+shoppinglist.delete_product(product)
+print(shoppinglist.products)
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    bought = db.Column(db.Boolean, nullable=False)
+
+    def __repr__(self):
+        return f"{self.name} - {self.price}"
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- src/shoppingList.py
+++ src/shoppingList.py
@@ -6,3 +6,10 @@ class ShoppingList:
     def add_product(self, product,amount,bought=False):
         new_product = pd.DataFrame([[product,amount, product.price*amount,bought]], columns=["product", "amount", "price","bought"])
         self.products = pd.concat([self.products, new_product], ignore_index=True)
+    
+    def edit_product(self, product,amount,bought=False):
+        self.products.loc[self.products["product"] == product, ["amount","price","bought"]] = [amount,product.price*amount,bought]
+
+    def delete_product(self, product):
+        self.products = self.products[self.products["product"] != product]
+        self.products.reset_index(drop=True, inplace=True)
