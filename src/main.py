49433931f1f743b59248dd6dd1dd9854b11196cc
juchim2005 <63352963+juchim2005@users.noreply.github.com>
2025-05-21 20:20:44 +0200

    First frontend update

--- src/main.py
+++ src/main.py
@@ -2,21 +2,9 @@ from shoppingList import *
 from product import *
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-shoppinglist = ShoppingList()
-product = Product("milk",1.99,"dairy",False)
-product2 = Product("eggs",0.99,"dairy",False)
-shoppinglist.add_product(product,2)
-shoppinglist.add_product(product2,3)
-product.price = 2.99
-shoppinglist.edit_product(product,3,True)
-print(shoppinglist.products)
-shoppinglist.delete_product(product)
-print(shoppinglist.products)
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
-
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
